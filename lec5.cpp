985fbb166cb7c3c38be86f9692038a581198b98d
Abhay Pandey <abhaypandey.1144@gmail.com>
2026-06-06 11:22:47 +0530

    Create lec5.cpp

---
+++ lec5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+   /* << is called insertion operator*/ 
+   cout<<"Enter the value of num1:\n";
+   cin>>num1; // << is called extraction operator;
+   cout<<"Enter the value of num2: \n";
+   cin>>num2;
+   cout<<"The sum is : \n" <<num1+num2;
+}

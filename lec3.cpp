9e7ea9139e14c75aa1a8d882c9c6b6929651995c
Abhay Pandey <abhaypandey.1144@gmail.com>
2026-06-06 11:22:22 +0530

    Create lec3.cpp

---
+++ lec3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    // this program is being written by me to show comment
+ 
+    /*THIS
+    IS TO SHOW MULTI LINE
+    COMMENT */
+
+    int sum = 9; // sum variable of int datatype
+
+    
+    cout<<sum;
+
+
+}
